c8f9d89c8415a79544308e03a97b305726946e68
20Simran <simran2000uk@gmail.com>
2025-07-17 16:51:05 +0530

    Initial Commit

---
+++ GitBrowser/GitBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitBrowserApp.swift
+//  GitBrowser
+//
+//  Created by Simran Semwal on 17/07/25.
+//
+
+import SwiftUI
+
+@main
+struct GitBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
